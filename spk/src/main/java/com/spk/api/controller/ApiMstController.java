70e9425ef56f81c70dee9486336224fccbce1160
JDY <doyoung.jin@gmail.com>
2022-08-08 23:37:35 +0900

    0808_3 API_MST : getAll(전체 리스트) 과 getByApiId(한건 조회) 에 대하여, Util 적용은 아니고, 로컬 소스에서 Json 포맷 가완성

--- spk/src/main/java/com/spk/api/controller/ApiMstController.java
+++ spk/src/main/java/com/spk/api/controller/ApiMstController.java
@@ -13,12 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.spk.api.entity.ApiMst;
 import com.spk.api.mapper.ApiMstMapper;
+import com.spk.api.util.Utils;
 //import com.spk.api.security.SignVerifier;
 
 @RestController
-@RequestMapping("/api")
+//@RequestMapping("/api")
+@RequestMapping(value = "/api", produces = "application/json; charset=utf8")
 public class ApiMstController {
 	
 	@Autowired 
@@ -95,24 +99,167 @@ public class ApiMstController {
 	//-------------------------------------------------------------------------------------------------------------------------------------
 	// list
 	@GetMapping("")
-	public List<ApiMst> getAll() {
+	public String getAll() {
 
-//	public List<ApiMst> getAll(@PathVariable("api_key") String api_key) {		
-//		SignVerifier verifier = new SignVerifier();
-//		
-//		  if (!verifier.verifySignature(api_key)) {
-//		//		response.sendError(HttpServletResponse.SC_NOT_FOUND, "주어진 키에 해당하는 파일 정보가 없습니다.");
-//				return null;
-//		  }		
+
+
+//		Utils utils = new Utils();
 		
-		return apimstMapper.getAll();
+		// Return할 최종 결과값		
+		JsonObject dataResult = new JsonObject();
+		JsonObject jsonObj1 = new JsonObject();
+		JsonArray jsonArr1 = new JsonArray();		
+		
+		String Message = "SUCCESS";
+		dataResult.addProperty("reason", Message);
+		dataResult.addProperty("result", "1");		
+		
+		List<ApiMst> datas = (List<ApiMst>) apimstMapper.getAll();		
+		
+		for (ApiMst item : datas) {
+		System.out.println("item==>"+item);
+		
+			jsonObj1.addProperty("reg_dt", item.getReg_dt());
+			jsonObj1.addProperty("reg_id", item.getReg_id());
+			jsonObj1.addProperty("upt_dt", item.getUpt_dt());
+			jsonObj1.addProperty("upt_id", item.getUpt_id());
+			jsonObj1.addProperty("api_nm", item.getApi_nm());
+			jsonObj1.addProperty("api_cate", item.getApi_cate());
+			jsonObj1.addProperty("version", item.getVersion());
+			jsonObj1.addProperty("param", item.getParam());
+			jsonObj1.addProperty("res_form", item.getRes_form());		
+			jsonObj1.addProperty("rtn_type", item.getRtn_type());
+			jsonObj1.addProperty("method", item.getMethod());
+			jsonObj1.addProperty("url", item.getUrl());
+			jsonObj1.addProperty("proc_state", item.getProc_state());
+			jsonObj1.addProperty("proc_rate", item.getProc_rate());
+			jsonObj1.addProperty("dev_fr_dt", item.getDev_fr_dt());
+			jsonObj1.addProperty("dev_to_dt", item.getDev_to_dt());
+			jsonObj1.addProperty("requester", item.getRequester());
+			jsonObj1.addProperty("owner", item.getOwner());
+			jsonObj1.addProperty("developer", item.getDeveloper());
+			jsonObj1.addProperty("participant", item.getParticipant());
+			jsonObj1.addProperty("scenario", item.getScenario());
+			jsonArr1.add(jsonObj1);		
+			
+	//		JsonObject lineData = new JsonObject();
+	//		lineData.add("result", jsonArr1);
+			dataResult.add("data", jsonArr1);		
+
+		}		
+		
+//		try {
+//			result = utils.getMultiGenerator(apimstMapper.getAll().toString());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+				
+		return dataResult.toString();
+//		return apimstMapper.getByApiId(api_id).toString();
 	}
+	
+//	// list
+//	@GetMapping("")
+//	public List<ApiMst> getAll() {
+//
+////	public List<ApiMst> getAll(@PathVariable("api_key") String api_key) {		
+////		SignVerifier verifier = new SignVerifier();
+////		
+////		  if (!verifier.verifySignature(api_key)) {
+////		//		response.sendError(HttpServletResponse.SC_NOT_FOUND, "주어진 키에 해당하는 파일 정보가 없습니다.");
+////				return null;
+////		  }		
+//		
+//		return apimstMapper.getAll();
+//	}
+	
+//	//================TRY(1)===================================
+//	// one
+//	@GetMapping(value = "/{api_id}")
+//	public String getByApiId(@PathVariable("api_id") String api_id) {
+//		
+//		// Return할 최종 결과값
+//		JsonObject result = new JsonObject();
+//		Utils utils = new Utils();
+//		
+//		System.out.println("123@@=>"+apimstMapper.getByApiId(api_id));
+//		
+//		try {
+//			result = utils.getOneGenerator(apimstMapper.getByApiId(api_id).toString());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//				
+//		return result.toString();
+////		return apimstMapper.getByApiId(api_id).toString();
+//	}	
+//	//================TRY(1)===================================
+	
+	//================TRY(2)===================================
 	// one
-	@GetMapping("/{api_id}")
-	public ApiMst getByApiId(@PathVariable("api_id") String api_id) {
-		System.out.println("123=>"+apimstMapper.getByApiId(api_id));
-		return apimstMapper.getByApiId(api_id);
-	}
+	@GetMapping(value = "/{api_id}")
+	public String getByApiId(@PathVariable("api_id") String api_id) {
+		
+		
+		System.out.println("[ApiMstController]getByApiId=>"+apimstMapper.getByApiId(api_id));
+		
+		ApiMst apimst = apimstMapper.getByApiId(api_id);
+		
+		
+		JsonObject dataResult = new JsonObject();
+		
+		String Message = "SUCCESS";
+		dataResult.addProperty("reason", Message);
+		dataResult.addProperty("result", "1");			
+		
+		
+		JsonObject jsonObj1 = new JsonObject();
+		JsonArray jsonArr1 = new JsonArray();
+		
+		jsonObj1.addProperty("reg_dt", apimst.getReg_dt());
+		jsonObj1.addProperty("reg_id", apimst.getReg_id());
+		jsonObj1.addProperty("upt_dt", apimst.getUpt_dt());
+		jsonObj1.addProperty("upt_id", apimst.getUpt_id());
+		jsonObj1.addProperty("api_nm", apimst.getApi_nm());
+		jsonObj1.addProperty("api_cate", apimst.getApi_cate());
+		jsonObj1.addProperty("version", apimst.getVersion());
+		jsonObj1.addProperty("param", apimst.getParam());
+		jsonObj1.addProperty("res_form", apimst.getRes_form());		
+		jsonObj1.addProperty("rtn_type", apimst.getRtn_type());
+		jsonObj1.addProperty("method", apimst.getMethod());
+		jsonObj1.addProperty("url", apimst.getUrl());
+		jsonObj1.addProperty("proc_state", apimst.getProc_state());
+		jsonObj1.addProperty("proc_rate", apimst.getProc_rate());
+		jsonObj1.addProperty("dev_fr_dt", apimst.getDev_fr_dt());
+		jsonObj1.addProperty("dev_to_dt", apimst.getDev_to_dt());
+		jsonObj1.addProperty("requester", apimst.getRequester());
+		jsonObj1.addProperty("owner", apimst.getOwner());
+		jsonObj1.addProperty("developer", apimst.getDeveloper());
+		jsonObj1.addProperty("participant", apimst.getParticipant());
+		jsonObj1.addProperty("scenario", apimst.getScenario());
+		jsonArr1.add(jsonObj1);		
+		
+//		JsonObject lineData = new JsonObject();
+//		lineData.add("result", jsonArr1);
+		dataResult.add("data", jsonArr1);		
+		
+//		String[] arrApiMst = new String[apimst. ];
+		
+		
+				
+		return dataResult.toString();
+//		return apimstMapper.getByApiId(api_id).toString();
+	}	
+	//================TRY(2)===================================	
+	
+	
+	
+//	// one
+//	@GetMapping("/{api_id}")
+//	public ApiMst getByApiId(@PathVariable("api_id") String api_id) {
+//		System.out.println("123=>"+apimstMapper.getByApiId(api_id));
+//		return apimstMapper.getByApiId(api_id);
+//	}
 	
 	//-------------------------------------------------------------------------------------------------------------------------------------
 	// UPDATE
