679227c3c1364c019dd3163ce1a31aeb3362f688
JDY <doyoung.jin@gmail.com>
2022-08-08 23:34:47 +0900

    0808_2 API_MST - Util 도 우선 Commit

---
+++ spk/src/main/java/com/spk/api/util/Utils.java
@@ -0,0 +1,42 @@
+package com.spk.api.util;
+
+import com.google.gson.JsonObject;
+
+public class Utils {
+
+	public JsonObject getOneGenerator(String data) throws Exception {
+		
+		
+		System.out.println("1. data ====> " + data);
+		
+		JsonObject dataResult = new JsonObject();
+		
+		try {	
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+		return dataResult;
+	
+	}
+	
+	
+	public JsonObject getMultiGenerator(String datas) throws Exception {
+		
+		
+		System.out.println("2. datas ====> " + datas);
+		
+		JsonObject dataResult = new JsonObject();
+		
+		try {	
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+		return dataResult;
+	
+	}	
+	
+}
