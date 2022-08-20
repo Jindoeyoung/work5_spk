b549e84d8bf3c9a7cf21c0acb07647121f419ddd
JDY <doyoung.jin@gmail.com>
2022-08-21 02:15:20 +0900

    0821_2 0820_1 관련, 함께 올렸어야 할 파일, 늦게 commit (Entity : ComApiRel2.java)

---
+++ spk/src/main/java/com/spk/api/entity/ComApiRel2.java
@@ -0,0 +1,14 @@
+package com.spk.api.entity;
+
+import lombok.Data;
+
+@Data
+public class ComApiRel2 {
+//	private String com_id;
+//	private String api_id;
+	private String api_src;
+//	private String reg_dt;
+//	private String reg_id;
+//	private String upt_dt;
+//	private String upt_id;
+}
