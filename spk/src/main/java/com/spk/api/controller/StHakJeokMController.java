40d0b3660b95395db002f216d7db1d9ff08adf90
JDY <doyoung.jin@gmail.com>
2022-10-18 10:25:51 +0900

    1005_1 - student_info 에 bigo 빠져서 추가 - 칼럼정보 조회 완성

--- spk/src/main/java/com/spk/api/controller/StHakJeokMController.java
+++ spk/src/main/java/com/spk/api/controller/StHakJeokMController.java
@@ -46,9 +46,8 @@ public class StHakJeokMController {
 	}	
 	
 	//-------------------------------------------------------------------------------------------------------------------------------------
-	// SELECT
+	// SELECT - LIST
 	//-------------------------------------------------------------------------------------------------------------------------------------
-	// list
 	@PostMapping("/student-list")
 	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	public String getStudentList(@RequestBody StHakJeokM _hakjeokm) throws Exception  {
@@ -102,7 +101,9 @@ public class StHakJeokMController {
 	
 	
 	
-	// one
+	//-------------------------------------------------------------------------------------------------------------------------------------
+	// SELECT - ONE
+	//-------------------------------------------------------------------------------------------------------------------------------------
 	@PostMapping("/student-info")
 	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	public String getByHakbeon(@RequestBody StHakJeokM _hakjeokm) throws Exception {
@@ -156,6 +157,7 @@ public class StHakJeokMController {
 			Obj1.addProperty("byunguk_gb", hakjeokm.getByunguk_gb());
 			Obj1.addProperty("joleop_jeungseo_no", hakjeokm.getJoleop_jeungseo_no());
 			Obj1.addProperty("hakwi_no", hakjeokm.getHakwi_no());
+			Obj1.addProperty("bigo", hakjeokm.getBigo());
 			
 			jsonArr1.add(Obj1);		
 			
@@ -205,8 +207,80 @@ public class StHakJeokMController {
 		return 0;
 	}	
 	
+	//-------------------------------------------------------------------------------------------------------------------------------------
+	// SELECT 칼럼 헤더명
+	//-------------------------------------------------------------------------------------------------------------------------------------
+	@PostMapping("/hakjeok-column-name")
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
+	public String getKorColumnName(@RequestBody StHakJeokM _hakjeokm) throws Exception {
+		
+		if (!authcheck.getMetaAuthErrGenerator(_hakjeokm.getApikey()).equals("{}")) {
+			logger.info("[StHakJeokMController][getByHakbeon] AUTHENTICATION RESTRICTIONS");
+			return authcheck.getMetaAuthErrGenerator(_hakjeokm.getApikey());
+		}		
+
+		StHakJeokM hakjeokm = sthakjeokmmapper.getKorColumnName(_hakjeokm.getHakbeon());
+
+		JsonObject dataResult = new JsonObject();
+		JsonArray jsonArr1 = new JsonArray();
+
+		String Message = "SUCCESS";
+		dataResult.addProperty("reason", Message);
+		dataResult.addProperty("result", "1");			
+
+		JsonObject Obj1 = new JsonObject();
+		JsonObject Obj2 = new JsonObject();
+
+		if (hakjeokm != null) {
+			Obj1.addProperty("hakbeon", hakjeokm.getHakbeon());
+			Obj1.addProperty("profile", hakjeokm.getProfile());
+			Obj1.addProperty("jumin_no", hakjeokm.getJumin_no());
+			Obj1.addProperty("birthday", hakjeokm.getBirthday());
+			Obj1.addProperty("h_name", hakjeokm.getH_name());
+			Obj1.addProperty("c_nane", hakjeokm.getC_nane());
+			Obj1.addProperty("gukga", hakjeokm.getGukga());
+			Obj1.addProperty("e_name_last", hakjeokm.getE_name_last());
+			Obj1.addProperty("e_name", hakjeokm.getE_name());
+			Obj1.addProperty("sangtae", hakjeokm.getSangtae());
+			Obj1.addProperty("byeondongsayu", hakjeokm.getByeondongsayu());
+			Obj1.addProperty("byeondong_ilja", hakjeokm.getByeondong_ilja());
+			Obj1.addProperty("gwajeong_gb", hakjeokm.getGwajeong_gb());
+			Obj1.addProperty("juya_gb", hakjeokm.getJuya_gb());
+			Obj1.addProperty("hakgwa", hakjeokm.getHakgwa());
+			Obj1.addProperty("jeongong", hakjeokm.getJeongong());
+			Obj1.addProperty("haknyeon", hakjeokm.getHaknyeon());
+			Obj1.addProperty("ban", hakjeokm.getBan());
+			Obj1.addProperty("hakgi", hakjeokm.getHakgi());
+			Obj1.addProperty("isuhakgi", hakjeokm.getIsuhakgi());
+			Obj1.addProperty("iphak_hakgwa", hakjeokm.getIphak_hakgwa());
+			Obj1.addProperty("jeonhyeong_gb", hakjeokm.getJeonhyeong_gb());
+			Obj1.addProperty("wetak_company", hakjeokm.getWetak_company());
+			Obj1.addProperty("iphak_ilja", hakjeokm.getIphak_ilja());
+			Obj1.addProperty("joleop_haknyeon", hakjeokm.getJoleop_haknyeon());
+			Obj1.addProperty("joleop_hakgwa", hakjeokm.getJoleop_hakgwa());
+			Obj1.addProperty("iphak_juya", hakjeokm.getIphak_juya());
+			Obj1.addProperty("iphak_gb", hakjeokm.getIphak_gb());
+			Obj1.addProperty("byunguk_gb", hakjeokm.getByunguk_gb());
+			Obj1.addProperty("joleop_jeungseo_no", hakjeokm.getJoleop_jeungseo_no());
+			Obj1.addProperty("hakwi_no", hakjeokm.getHakwi_no());
+			Obj1.addProperty("bigo", hakjeokm.getBigo());
+			
+			jsonArr1.add(Obj1);		
+			
+			Obj2.add("result", jsonArr1);
+			dataResult.add("data", Obj2);		
+		} else {
+			dataResult.addProperty("data", "");
+		}
+		logger.info("getByHakbeon=>"+dataResult.toString());		
+		return dataResult.toString();
+	}	
 	
-	// one
+	
+	
+	//-------------------------------------------------------------------------------------------------------------------------------------
+	// SELECT - ONE (GET 방식)
+	//-------------------------------------------------------------------------------------------------------------------------------------
 //	@GetMapping("/studentinfo_get/{apikey},{hakbeon}")
 //	public String getByHakbeonGet(@PathVariable String apikey, @PathVariable String hakbeon) throws Exception {
 	@GetMapping("/studentinfo_get/apikey={apikey}&hakbeon={hakbeon}")
--- spk/src/main/java/com/spk/api/mapper/StHakJeokMMapper.java
+++ spk/src/main/java/com/spk/api/mapper/StHakJeokMMapper.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
@@ -91,8 +92,9 @@ public interface StHakJeokMMapper {
 			+ ")")	
 	int insertBody(@Param("ST_HAKJEOK_M") StHakJeokM hakjeokm);	
 	
-	
-	// select one
+	//-------------------------------------------------------------------------------------------------------------------------------------
+	// SELECT - ONE
+	//-------------------------------------------------------------------------------------------------------------------------------------	
 	@Select("SELECT * FROM ST_HAKJEOK_M WHERE hakbeon=#{hakbeon}")
 	@Results(id="HakJeokMap", value={
 		@Result(property="hakbeon",             column="hakbeon"),
@@ -125,9 +127,12 @@ public interface StHakJeokMMapper {
 		@Result(property="iphak_gb",            column="iphak_gb"),
 		@Result(property="byunguk_gb",          column="byunguk_gb"),
 		@Result(property="joleop_jeungseo_no",  column="joleop_jeungseo_no"),
-		@Result(property="hakwi_no",            column="hakwi_no")
+		@Result(property="hakwi_no",            column="hakwi_no"),
+		@Result(property="bigo",            	column="bigo")
 	})
 	StHakJeokM getByHakbeon(@Param("hakbeon") String hakbeon);
+	
+	
 	
 //	@Select("SELECT "
 //			+ "     hakbeon"
@@ -149,6 +154,10 @@ public interface StHakJeokMMapper {
 //			+ "   and hakbeon LIKE CONCAT('%', #{ST_HAKJEOK_M.hakbeon}, '%')")			
 ////			+ " WHERE hakbeon LIKE CONCAT(#{ST_HAKJEOK_M.hakbeon}, '%')")
 	
+	
+	//-------------------------------------------------------------------------------------------------------------------------------------
+	// SELECT - LIST
+	//-------------------------------------------------------------------------------------------------------------------------------------	
 	@Select("SELECT "
 			+ "     HJ.hakbeon"
 			+ "    ,HJ.haknyeon"
@@ -226,5 +235,80 @@ public interface StHakJeokMMapper {
 	// DELETE
 	//-------------------------------------------------------------------------------------------------------------------------------------
 	@Delete("DELETE FROM ST_HAKJEOK_M WHERE hakbeon=#{ST_HAKJEOK_M.hakbeon}")
-	int delete(@Param("ST_HAKJEOK_M") StHakJeokM hakjeokm);	
+	int delete(@Param("ST_HAKJEOK_M") StHakJeokM hakjeokm);
+	
+	
+	//-------------------------------------------------------------------------------------------------------------------------------------
+	// SELECT 칼럼 헤더명
+	//-------------------------------------------------------------------------------------------------------------------------------------	
+	// select 
+	@Select("SELECT  max(case COLUMN_NAME "
+			+ "		when 'HAKBEON' then '학번' end) as HAKBEON, "
+			+ "        max(case COLUMN_NAME "
+			+ "	    when 'PROFILE' then '프로필' end) as PROFILE, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'JUMIN_NO' then '주민번호' end) as JUMIN_NO, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'BIRTHDAY' then '생년월일' end) as BIRTHDAY, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'H_NAME' then '성명' end) as H_NAME, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'C_NANE' then '한문성명' end) as C_NANE, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'GUKGA' then '국가' end) as GUKGA, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'E_NAME_LAST' then '영문성' end) as E_NAME_LAST, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'E_NAME' then '영문이름' end) as E_NAME, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'SANGTAE' then '학생상태' end) as SANGTAE, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'BYEONDONGSAYU' then '변동사유' end) as BYEONDONGSAYU, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'BYEONDONG_ILJA' then '변동일자' end) as BYEONDONG_ILJA, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'GWAJEONG_GB' then '과정구분' end) as GWAJEONG_GB, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'JUYA_GB' then '주야구분' end) as JUYA_GB, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'HAKGWA' then '학과' end) as HAKGWA, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'JEONGONG' then '전공' end) as JEONGONG, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'HAKNYEON' then '학년' end) as HAKNYEON, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'BAN' then '학반' end) as BAN, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'HAKGI' then '학기' end) as HAKGI, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'ISUHAKGI' then '이수학기' end) as ISUHAKGI, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'IPHAK_HAKGWA' then '입학학과' end) as IPHAK_HAKGWA, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'JEONHYEONG_GB' then '전공구분' end) as JEONHYEONG_GB, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'WETAK_COMPANY' then '위탁회사' end) as WETAK_COMPANY, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'IPHAK_ILJA' then '입학일자' end) as IPHAK_ILJA, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'JOLEOP_HAKNYEON' then '졸업학년' end) as JOLEOP_HAKNYEON, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'JOLEOP_HAKGWA' then '졸업학과' end) as JOLEOP_HAKGWA, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'IPHAK_JUYA' then '입학주야' end) as IPHAK_JUYA, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'IPHAK_GB' then '입학구분' end) as IPHAK_GB, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'BYUNGUK_GB' then '병역사항' end) as BYUNGUK_GB, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'JOLEOP_JEUNGSEO_NO' then '졸업증서번호' end) as JOLEOP_JEUNGSEO_NO, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'HAKWI_NO' then '학위번호' end) as HAKWI_NO, "
+			+ "        max(case COLUMN_NAME "
+			+ "		when 'BIGO' then '비고' end) as BIGO "
+			+ " FROM    INFORMATION_SCHEMA.COLUMNS "
+			+ " WHERE   TABLE_NAME = 'ST_HAKJEOK_M' "
+			+ " AND   TABLE_SCHEMA = 'spike' ")
+	@ResultMap("HakJeokMap")
+	StHakJeokM getKorColumnName(@Param("hakbeon") String hakbeon);	
 }
