35be033a16436789c6998908478a1359c64bac93
JDY <doyoung.jin@gmail.com>
2022-11-01 18:14:34 +0900

    1101_1 - 컴포넌트 시나리오 조회 관련 . CORS 적용

--- spk/src/main/java/com/spk/api/controller/ComMstController.java
+++ spk/src/main/java/com/spk/api/controller/ComMstController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -260,6 +261,7 @@ public class ComMstController {
 	 * @throws Exception e 
 	 */ 
 	@PostMapping("/scenario-info")
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	public String getScenarioByComId(@RequestBody ComMst _commst) throws Exception {
 		
 		if (!authcheck.getMetaAuthErrGenerator(_commst.getApikey()).equals("{}")) {
