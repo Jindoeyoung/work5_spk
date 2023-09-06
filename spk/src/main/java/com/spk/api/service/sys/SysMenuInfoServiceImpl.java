d6c5616bb571dcee5371cf2e7c76ffc3b514b545
Jindoeyoung <doyoung.jin@gmail.com>
2023-09-06 20:54:44 +0900

    230906_6 - 시스템 메뉴 관리 - INSERT 구현  . 저장 후 저장데이터 Json Return 완성  . 소스 정리 필요

--- spk/src/main/java/com/spk/api/service/sys/SysMenuInfoServiceImpl.java
+++ spk/src/main/java/com/spk/api/service/sys/SysMenuInfoServiceImpl.java
@@ -163,62 +163,42 @@ public class SysMenuInfoServiceImpl implements SysMenuInfoService {
         //============================================================		
 		JsonObject dataResult = new JsonObject();
 		JsonArray jsonArr1 = new JsonArray();
-		JsonArray jsonArr2 = new JsonArray();
-		JsonObject Obj1 = new JsonObject();
-		JsonObject Obj2 = new JsonObject();
-		JsonObject Obj3 = new JsonObject();	
-		
-		String OK_MESSAGE = "SUCCESS";
-		String NOT_OK_MESSAGE = "FAIL";
+		String Message = "SUCCESS";
+		String Success = "1";
 		
 		try {
 			int results = sysMenuInfoMapper.insertSysMenuInfo(pSysMenuInfo);
 
-//	        //============================================================
-//	        //< Respnse Data (Sample)
-//			//< API 호출 시 공통으로 리턴받는 Response Data
-//	        //============================================================			
-//			String if_id = "IF-001-001";				// API id
-//			String version = "v1.0";					// API version
-//			String m_code = "HAK000";					// Menu Code
-//			String s_code = "HAK001";					// Sub Menu Code
-//			String response_format = "json";			// 반환 data 타입
-//			String result = "";							// 응답코드 4자리
-//			String reason = "";							// 응답 결과 메세지 ("성공", "필수항목 누락" 등)
-//			String server_time = "2021-04-13 15:40:40";	//응답 시 서버 시간
-//			String response_time = "132";				// 서버에서 처리한 경과 시간(milliseconds)
-//			// 인터페이스별로 반환되는 데이터가 있을 경우 "datas" Object 하위로 반환
-//			// 추가 반환할 데이터가 없을 경우 null 또는 ""(빈스트링) 반환
-//			
-//			if (results == 1) {
-//				dataResult.addProperty("reason", OK_MESSAGE);
-//				result = "0000";
-//				reason = "성공";
-//			} else {
-//				dataResult.addProperty("reason", NOT_OK_MESSAGE);
-//				result = "0001";	
-//				reason = "필수항목누락";				
-//			}
-//			dataResult.addProperty("result", results);
-//			
-//			Obj1.addProperty("if_id", if_id);
-//			Obj1.addProperty("version", version);
-//			Obj1.addProperty("m_code", m_code);
-//			Obj1.addProperty("s_code", s_code);
-//			Obj1.addProperty("response_format", response_format);
-//			Obj1.addProperty("result", result);
-//			Obj1.addProperty("reason", reason);
-//			Obj1.addProperty("server_time", server_time);
-//			Obj1.addProperty("response_time", response_time);
-//
-//			Obj2.addProperty("hakbeon", pUhj01Entity.getHakbeon());
-//			jsonArr2.add(Obj2);
-//			Obj1.add("datas", jsonArr2);			
-//			
-//			jsonArr1.add(Obj1);
-//			Obj3.add("result", jsonArr1);
-//			
-//			dataResult.add("data", Obj3);
+			
+			if (results == 1) {
+			
+				SysMenuInfo sysMenuInfo = sysMenuInfoMapper.getSysMenuInfo(pSysMenuInfo);
+				dataResult.addProperty("reason", Message);
+				dataResult.addProperty("result", Success);
+				
+				if (sysMenuInfo != null) {
+					
+						JsonObject Obj1 = new JsonObject();
+						
+						Obj1.addProperty("spike_id", sysMenuInfo.getSpike_id());
+						Obj1.addProperty("commenter", sysMenuInfo.getCommenter());
+						Obj1.addProperty("createdAt", sysMenuInfo.getReg_dt());
+						Obj1.addProperty("request", sysMenuInfo.getRequest());
+						Obj1.addProperty("use_yn", sysMenuInfo.getUse_yn());
+						
+						jsonArr1.add(Obj1);
+						
+						dataResult.add("flag_info", jsonArr1);
+
+				} else {
+					JsonObject Obj3 = new JsonObject();
+					Obj3.add("result", jsonArr1);
+					dataResult.add("data", Obj3);
+				}
+			
+			} // if (results == 1) {
+			
+			
 		} catch (Exception e) {
 			logger.error("[UHJ01ServiceImpl.insertStudent] ERROR : " + e);
 			// TODO
